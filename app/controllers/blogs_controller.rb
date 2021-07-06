02bc0e9de4d38d38495541db71ee1979f1bfec84
ykawa727 <y.aikawa727@gmail.com>
2021-07-06 02:42:08 +0000

    learned debug

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    # p "標準出力にのみ反映"
+    # logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -13,6 +15,7 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
+    # binding.pry
     blog.save
     redirect_to blog_path(blog.id)
   end
